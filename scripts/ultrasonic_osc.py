b60f514e02a9edb21b56ad01f42efc1e18b5d862
Gilbert <gilbert.sinnott@mailbox.org>
2019-01-24 17:15:25 +0000

    Ultrasonic fixes

--- scripts/ultrasonic_osc.py
+++ scripts/ultrasonic_osc.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import time
 from oscpy.client import OSCClient
 
+maxDistance = 400.0
+minDistance = 10.0
 
 try:
       osc = OSCClient('127.0.0.1', 7000)
@@ -15,6 +17,7 @@ try:
 
       GPIO.setup(PIN_TRIGGER, GPIO.OUT)
       GPIO.setup(PIN_ECHO, GPIO.IN)
+      GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
       GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
@@ -25,19 +28,20 @@ try:
 
       print "Calculating distance"
       while True:
-
-
-            timestamp = time.time()
-            sendValue = True
+            if GPIO.input(10) == GPIO.HIGH:
+                  print("Button was pushed!")
+                  osc.send_message(b'/Button', [1])
 
 
             GPIO.output(PIN_TRIGGER, GPIO.HIGH)
             time.sleep(0.0001)
             GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-            maxTime = 400.0 / 17150.0
+            maxTime = maxDistance / 17150.0
             
             loop_start_time = time.time()
+            pulse_start_time = 0
+            pulse_end_time = 0            
 
             # print ">"
 
@@ -46,27 +50,29 @@ try:
             while GPIO.input(PIN_ECHO)==0:
                   pulse_start_time = time.time()
                   if ( (pulse_start_time - loop_start_time) > maxTime ):
-                        skipped = " START SKIPPED"
+                        skipped = " Start skipped"
                         break
             while GPIO.input(PIN_ECHO)==1:
                   pulse_end_time = time.time()
                   if ( (pulse_end_time - pulse_start_time) > maxTime ):
-                        skipped = " END SKIPPED"
+                        skipped = " End skipped"
                         pulse_end_time = pulse_start_time + maxTime
                         break
 
             pulse_duration = pulse_end_time - pulse_start_time
             distance = round(pulse_duration * 17150, 1)
 
-            if (sendValue):
+            # if (skipped == ""):
+            # osc.send_message(b'/Ultrasonic', [distance])
+
+            if (distance < maxDistance) & (distance >= minDistance):
+                  print "Distance:", distance, "cm", skipped
 
                   osc.send_message(b'/Ultrasonic', [distance])
-                  print "Distance:", distance, "cm", skipped
-            else:
-                  print "Skipping no returned echo"
 
             time.sleep(0.05)
             
 except KeyboardInterrupt:
       print("Measurement stopped by User")
       GPIO.cleanup()
+
